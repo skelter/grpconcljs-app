f88d52234fd25ab22a154619e3c389b1982fd1f0
Steve Suehs <skelter.net@gmail.com>
2022-02-01 11:15:47 -0600

    Results after first whack.

---
+++ protos.out/entityeventcode.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.entityevents.EntityEventCode');
+
+/**
+ * @enum {number}
+ */
+proto.entityevents.EntityEventCode = {
+  ENTITIESDUMP: 0,
+  ENTITYCREATED: 1,
+  ENTITYDESTROYED: 2,
+  ENTITYUPDATED: 3,
+  ASSOC: 4,
+  DISSOC: 5
+};
+
