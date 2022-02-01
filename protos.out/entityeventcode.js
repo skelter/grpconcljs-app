7a3b7a3e9e88af0d0f46a02c68f9e550adac4aa7
Steve Suehs <skelter.net@gmail.com>
2022-02-01 11:24:48 -0600

    Generated with 3.19.4

--- protos.out/entityeventcode.js
+++ protos.out/entityeventcode.js
@@ -1,9 +1,15 @@
+// source: protos/entityevents.proto
 /**
  * @fileoverview
  * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
 
 goog.provide('proto.entityevents.EntityEventCode');
 
